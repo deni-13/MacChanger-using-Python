09c279a7be2539fda2ace567b795b15184c2ba2c
Fidelio <78764084+ethereal03@users.noreply.github.com>
2021-04-22 14:43:15 +0300

    Delete interfaces.py

--- interfaces.py
+++
@@ -1,9 +0,0 @@
-#mymacchanger
-import subprocess
-print("mac changer started!")
-
-interface="eth0"
-mac_adress="00:22:33:77:99:11"
-subprocess.call(["ifconfig","eth0","down"])
-subprocess.call(["ifconfig","eth0","hw","ether",mac_adress])
-subprocess.call(["ifconfig","eth0","up"])
